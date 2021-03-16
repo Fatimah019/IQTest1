c723c4a7d20a456cb4b47904a657ddee80e1505a
Fatimah <daviesfatimah97@gmail.com>
2021-03-16 09:51:48 -0700

    taskone front end trask

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./ReduxSetup/Store";
-import TaskFour from "./Components/TaskFour";
-import TaskOne from "./Components/TaskOne/index";
-import PageTwo from "./Components/TaskOne/pagetwo";
-import PageThree from "./Components/TaskOne/pagethree";
-import PageFour from "./Components/TaskOne/pagefour";
+import TaskFour from "./Components/CovidCases";
 
 function App() {
   return (
@@ -15,10 +11,6 @@ function App() {
         <Provider store={store}>
           <Switch>
             <Route path="/covid-cases" component={TaskFour} />
-            <Route path="/" exact component={TaskOne} />
-            <Route path="/billing" component={PageTwo} />
-            <Route path="/confirm-payment" component={PageThree} />
-            <Route path="/payment-complete" component={PageFour} />
           </Switch>
         </Provider>
       </Router>
